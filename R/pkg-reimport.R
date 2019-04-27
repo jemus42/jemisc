02c6dbf9efca688a0e405df2c385195da304a79e
Lukas Burk <lukas@quantenbrot.de>
2019-04-28 00:38:28 +0200

    Make us of pak

--- R/pkg-reimport.R
+++ R/pkg-reimport.R
@@ -1,19 +1,27 @@
 #' Export installed packages for somehwat functional reinstallation
 #'
+#' @param old_lib `character`: The location of your previous package library.
 #' @export
-#'
+#' @importFrom pak lib_status
+#' @importFrom tibble as_tibble
+#' @importFrom glue glue_data
+#' @importFrom dplyr select
+#' @importFrom dplyr filter
 #' @examples
 #' \dontrun{
 #' pkg_export()
 #' }
-pkg_export <- function() {
-  pkgs  <- installed.packages(lib.loc = .libPaths()[[1]]) # Get packages
+pkg_export <- function(old_lib = "~/Library/R/shared_bak/") {
+  pak::lib_status(lib = old_lib) %>%
+    as_tibble() %>%
+    select(package, title, version, repository, remotetype, remoterepo, remoteusername) %>%
+    glue_data("if (!'{package}' %in% installed.packages()) remotes::install_cran('{package}')") %>%
+    writeLines("cran-reinstall.R")
 
-  pkgs_vec <- rownames(pkgs)
-  scrpt <- glue::glue("if (!('[pkgs_vec]' %in% installed.packages())) ",
-                      "install.packages('[pkgs_vec]')",
-                      .open = "[", .close = "]") # Assemble script
-
-  saveRDS(pkgs, "pkgs.rds")
-  writeLines(scrpt, con = "pkg-reimport.R")
+  pak::lib_status(lib = old_lib) %>%
+    as_tibble() %>%
+    select(package, title, version, repository, remotetype, remoterepo, remoteusername) %>%
+    filter(remotetype == "github") %>%
+    glue_data("if (!'{package}' %in% installed.packages()) remotes::install_github('{remoteusername}/{remoterepo}')") %>%
+    writeLines("github-reinstall.R")
 }
