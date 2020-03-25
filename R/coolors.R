a43beb15a77fe2ff24708414543cdfd12aea33df
Lukas Burk <lukas@quantenbrot.de>
2020-03-26 00:10:01 +0100

    Mildly smarter regex

--- R/coolors.R
+++ R/coolors.R
@@ -5,13 +5,12 @@
 #' @note It's only the URL that's parsed, there's no need for an internet connection.
 #' @return `character(5)` with RGB color codes, e.g.
 #' `c("#29adb2", "#182326", "#2a3a3f", "#69818c", "#8ea4b2")` for the example `url`.
-#' @importFrom stringr str_remove str_extract_all str_pad
+#' @importFrom stringr str_extract_all str_pad
 #' @export
 #' @examples
 #' coolors_pal("https://coolors.co/29adb2-182326-2a3a3f-69818c-8ea4b2")
 coolors_pal <- function(url) {
   url %>%
-    str_remove("^.*\\.co\\/") %>%
-    str_extract_all("([a-z0-9]){6}", simplify = TRUE) %>%
+    str_extract_all("([a-f0-9]){6}", simplify = TRUE) %>%
     str_pad(7, "left", "#")
 }
